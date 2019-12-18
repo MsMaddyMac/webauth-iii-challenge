9b485b5966e5402fa3523df8f42b44a290ea71ba
Maddy <mmcintosh2k14@gmail.com>
2019-12-18 17:23:29 -0500

    creates restriction on GET endpoint

--- helpers/signToken.js
+++ helpers/signToken.js
@@ -1,15 +1,14 @@
 const jwt = require('jsonwebtoken');
+const secret = require('../secrets/secrets');
 
 module.exports = function signToken(user) {
 	const payload = {
 		username: user.username
 	};
 
-	const secret = process.env.JWT_SECRET || 'pinky promise, please!';
-
 	const options = {
 		expiresIn: '1hr'
 	};
 
-	return jwt.sign(payload, secret, options);
+	return jwt.sign(payload, secret.jwtSecret, options);
 };
