91959711d5038369702b632d5b53c5a4cca98399
Maddy <mmcintosh2k14@gmail.com>
2019-12-18 16:38:41 -0500

    creates POST endpoint to register new user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,4 +2,22 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const users = require('../users/users-model');
+const Users = require('../users/users-model');
+
+// for all endpoints beginning with /api/auth
+router.post('/register', (req, res) => {
+	let user = req.body;
+	const hash = bcrypt.hashSync(user.password, 10);
+	user.password = hash;
+
+	Users.add(user)
+		.then(saved => {
+			res.status(201).json(saved);
+		})
+		.catch(err => {
+			console.log('Error registering user.', err);
+			res.status(500).json({ error: 'Error registering user.' });
+		});
+});
+
+module.exports = router;
