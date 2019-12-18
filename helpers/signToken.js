3d021adae115cdee1c0023f43f1b1677f29fbbeb
Maddy <mmcintosh2k14@gmail.com>
2019-12-18 16:53:19 -0500

    creates POST endpoint to login user and signToken helpers function

---
+++ helpers/signToken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function signToken(user) {
+	const payload = {
+		username: user.username,
+		department: user.department
+	};
+
+	const secret = process.env.JWT_SECRET || 'pinky promise, please!';
+
+	const options = {
+		expiresIn: '1hr'
+	};
+
+	return jwt.sign(payload, secret, options);
+};
