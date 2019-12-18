d8da8abe5e298a8f13addc5c4da02d155e5b1301
Maddy <mmcintosh2k14@gmail.com>
2019-12-18 17:24:11 -0500

    adds console.log to GET endpoint for error

--- users/users-router.js
+++ users/users-router.js
@@ -4,11 +4,14 @@ const Users = require('./users-model');
 const restricted = require('../auth/restricted-middleware');
 
 router.get('/', restricted, (req, res) => {
-    Users.find()
-        .then(users => {
-            res.json(users);
-        })
-        .catch(err => res.send(err));
-})
+	Users.find()
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => {
+			console.log('Not authorized to see users.', err);
+			res.json({ message: 'You shall not pass!' });
+		});
+});
 
 module.exports = router;
